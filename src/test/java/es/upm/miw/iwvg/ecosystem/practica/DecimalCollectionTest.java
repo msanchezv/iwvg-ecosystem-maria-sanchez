1b64e942997e07a25e8f6f7a84daf09a1ba43dc1
María Sánchez <masanchezve@gmail.com>
2019-09-23 21:51:53 +0200

    Issue #8 Add DecimalCollectionTest

---
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practica/DecimalCollectionTest.java
@@ -0,0 +1,40 @@
+package es.upm.miw.iwvg.ecosystem.practica;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DecimalCollectionTest {
+    private User user;
+
+    @BeforeEach
+    void before() {
+        user = new User(1, "mateo", "sánchez");
+    }
+
+    @Test
+    void testFormat() {
+        assertEquals("Mateo Sánchez", user.fullName());
+    }
+
+    @Test
+    void testInitials() {
+        assertEquals("M.", user.initials());
+    }
+
+    @Test
+    void testNumber() {
+        assertEquals(1, user.getNumber());
+    }
+
+    @Test
+    void testName() {
+        assertEquals("Mateo", user.getName());
+    }
+
+    @Test
+    void testFamilyName() {
+        assertEquals("Sánchez", user.getFamilyName());
+    }
+}
