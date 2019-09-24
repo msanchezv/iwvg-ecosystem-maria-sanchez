9fc35af5d11523bf745dcd16fc85ffaa42d42b4b
María Sánchez <masanchezve@gmail.com>
2019-09-24 19:42:43 +0200

    Issue #11 Add 1st Point improvement

--- src/test/java/es/upm/miw/iwvg/ecosystem/practica/PointTest.java
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practica/PointTest.java
@@ -10,13 +10,14 @@ class PointTest {
 
     @BeforeEach
     void before() {
-        point = new Point(2, 3);
+        point = new Point(2, 3, 4);
     }
 
     @Test
     void testPointIntInt() {
         assertEquals(2, point.getX());
         assertEquals(3, point.getY());
+        assertEquals(4, point.getZ());
     }
 
     @Test
@@ -24,6 +25,7 @@ class PointTest {
         point = new Point(2);
         assertEquals(2, point.getX());
         assertEquals(2, point.getY());
+        assertEquals(2, point.getZ());
     }
 
     @Test
@@ -31,23 +33,34 @@ class PointTest {
         point = new Point();
         assertEquals(0, point.getX());
         assertEquals(0, point.getY());
+        assertEquals(0, point.getZ());
     }
 
     @Test
     void testModule() {
-        assertEquals(3.6055, point.module(), 10e-5);
+        assertEquals(5.3851, point.module(), 10e-5);
     }
 
     @Test
     void testPhase() {
-        assertEquals(0.9828, point.phase(), 10e-5);
+        assertEquals(0.5880, point.phase(), 10e-5);
     }
 
     @Test
     void testTranslateOrigin() {
-        this.point.translateOrigin(new Point(1, 1));
+        this.point.translateOrigin(new Point(1, 1, 1));
         assertEquals(1, point.getX());
         assertEquals(2, point.getY());
+        assertEquals(3, point.getZ());
     }
 
+    @Test
+    void testToString() {
+        String pointStr = "Point{" +
+                "x=" + point.getX() +
+                ", y=" + point.getY() +
+                ", z=" + point.getZ() +
+                '}';
+        assertEquals(pointStr, point.toString());
+    }
 }
